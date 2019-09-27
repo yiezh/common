d732310f27dda7a42dc77ab0748d99ea78380fe3
enigma <luzhihaog@gmail.com>
2019-09-27 18:32:52 +0800

    完善日志相关内容

---
+++ src/main/java/cn/enigma/project/common/define/SystemDefine.java
@@ -0,0 +1,11 @@
+package cn.enigma.project.common.define;
+
+/**
+ * @author luzh
+ * Create: 2019/9/27 下午4:06
+ * Modified By:
+ * Description:
+ */
+public class SystemDefine {
+    public static final String HTTP_REQUEST_TRACE_ID = "X-B3-TraceId";
+}
