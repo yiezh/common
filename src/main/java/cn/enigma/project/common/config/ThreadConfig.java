a373fed89b5857711afb025286e6dd018b3b4173
enigma-ll <luzhihaog@gmail.com>
2019-10-18 00:08:58 +0800

    修改线程池的配置; 更改日志打印

--- src/main/java/cn/enigma/project/common/config/ThreadConfig.java
+++ src/main/java/cn/enigma/project/common/config/ThreadConfig.java
@@ -13,6 +13,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * remark
+ *
  * @author luzh
  * createTime 2017年6月23日 下午5:10:45
  */
@@ -20,37 +21,37 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Configuration
 public class ThreadConfig extends AsyncConfigurerSupport {
 
-	private final BeanFactory beanFactory;
+    private final BeanFactory beanFactory;
 
-	public ThreadConfig(BeanFactory beanFactory) {
-		this.beanFactory = beanFactory;
-	}
+    public ThreadConfig(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
 
-	@Bean
-	public Executor executor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		int corePoolSize = 10;
-		executor.setCorePoolSize(corePoolSize);
-		int maxPoolSize = 20;
-		executor.setMaxPoolSize(maxPoolSize);
-		int queueCapacity = 30000;
-		executor.setQueueCapacity(queueCapacity);
-		executor.setThreadNamePrefix("pool-thread-");
-		// rejection-policy：当pool已经达到max size的时候，如何处理新任务
-		// CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
-		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-		int awaitTerminationSeconds = 60;
-		executor.setAwaitTerminationSeconds(awaitTerminationSeconds);
-		// 设置线程池关闭的时候等待所有任务都完成再继续销毁其他的bean
-		executor.setWaitForTasksToCompleteOnShutdown(true);
-		executor.initialize();
-		Runtime.getRuntime().addShutdownHook(new Thread(executor::shutdown));
-		return new LazyTraceExecutor(beanFactory, executor);
-	}
+    @Bean
+    public Executor executor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        int corePoolSize = 10;
+        executor.setCorePoolSize(corePoolSize);
+        int maxPoolSize = 20;
+        executor.setMaxPoolSize(maxPoolSize);
+        int queueCapacity = 30000;
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix("pool-thread-");
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        int awaitTerminationSeconds = 60;
+        executor.setAwaitTerminationSeconds(awaitTerminationSeconds);
+        // 设置线程池关闭的时候等待所有任务都完成再继续销毁其他的bean
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        Runtime.getRuntime().addShutdownHook(new Thread(executor::shutdown));
+        return new LazyTraceExecutor(beanFactory, executor);
+    }
 
 
-	@Override
-	public Executor getAsyncExecutor() {
-		return executor();
-	}
+    @Override
+    public Executor getAsyncExecutor() {
+        return executor();
+    }
 }
